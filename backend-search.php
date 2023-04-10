d3dcad0d034c783ceca7f9bb58e3a879392c0df5
rihanciara <thufailmuhammed@gmail.com>
2023-04-10 14:15:54 +0530

    Update backend-search.php

--- backend-search.php
+++ backend-search.php
@@ -1,7 +1,7 @@
 <?php
 /* Attempt MySQL server connection. Assuming you are running MySQL
 server with default setting (user 'root' with no password) */
-$link = mysqli_connect("localhost", "test", "CodeLover247", "dbtest");
+$link = mysqli_connect("localhost", "test", "", "dbtest");
  
 // Check connection
 if($link === false){
